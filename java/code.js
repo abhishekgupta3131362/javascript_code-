90832c9ca04bc99915207292bec9b225137996ad
abhi the rockstar <guptaabhishek58433@gmail.com>
2026-09-05 06:04:44 +0000

    Add coding.js

---
+++ java/code.js
@@ -0,0 +1,15 @@
+console.log("virat kohli and mahendra  singh dhoni ");
+let account =324;
+const hi=832;
+var hello="abhi";
+console.table([account,hi,hello]);
+const user=[
+    {
+        name:"abhi",age :20
+    },
+    { name:" rahul",age:22},
+    { name: "salmon bhai", age:40}
+
+
+];
+console.table(user,["name"]);
---
+++ java/coding.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('alert');
+console.log(3+3);
